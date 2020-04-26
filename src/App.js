5aef6006f6706c5dbf54b4e195a75c937a8fea83
Gustavo Siciliano <gussiciliano@gmail.com>
2020-04-26 02:07:15 -0300

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import ConditionalSection from './sections/conditional'
+import Forms from './sections/forms'
 import cars from './data/cars.json'
 
 class CarItem extends Component{
@@ -15,26 +17,77 @@ class CarItem extends Component{
   }
 }
 
-function App() {
-  const numbers = [1, 1, 3 ,4 ,5]
-  return (
-    <div>
-      <ConditionalSection />
-      <div>
-        {numbers.map((number, index) => {
-          return <p key={index}>Soy el número {number}</p>})}
+class Box extends Component {
+  static propTypes = {
+    info: PropTypes.string.isRequired,
+    // date: PropTypes.string.isRequired
+    date: PropTypes.string
+  }
+  render(){
+    const { info, date } = this.props
+    return(
+      <div style={{ border: '1px solid #09f', margin: 10, padding: 10 }}>
+        <p>{info}</p>
+        <p>{date &&<em>la fecha es {date}</em>}</p>
+        {this.props.children}    
       </div>
+    )
+  }
+}
+
+const numbers = [1, 1, 3 ,4 ,5];
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = { mouseX: 0, mouseY: 0 };
+  }
+
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    this.setState( { mouseX: clientX, mouseY: clientY })
+  }
+
+  handleClick (e) {
+    console.log(e)
+    alert('Test 2')
+  }
+  render(){
+    return (
       <div>
-        <ul>
-          {
-            cars.map(car => {
-              return <CarItem key={car.id} car={car} />
-            })
-          }
-        </ul>
+        <Forms />
+        <ConditionalSection />
+        <div>
+          <button onClick={this.handleClick}>Test on clic</button>
+        </div>
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{ border: '1px solid #000', margin: 10, padding: 10 }}>
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
+        <Box
+          info='Info 1'
+          date={new Date().toLocaleDateString()}>
+            Test 1 children <strong>y puedo poner negritas si quiero</strong></Box>
+        <Box
+          info='Info 1'>
+            Test 2 children</Box>
+        <div>
+          {numbers.map((number, index) => {
+            return <p key={index}>Soy el número {number}</p>})}
+        </div>
+        <div>
+          <ul>
+            {
+              cars.map(car => {
+                return <CarItem key={car.id} car={car} />
+              })
+            }
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/sections/forms.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+
+class Forms extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name: '',
+      lastname: '',
+      dni: '',
+      termns: true
+    }
+  }
+
+  /*handleClick = (e) => {
+    e.preventDefault()
+    const name = this.inputName.value
+    const apellido = document.getElementById('apellido').value
+    const dni = document.getElementById('dni').value
+    console.log({name, apellido, dni})
+  }*/
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    //const name = this.inputName.value
+    //const apellido = document.getElementById('apellido').value
+    console.log(this.state)
+  }
+
+  handleChange = (e) => {
+    this.setState({ termns: e.target.checked })
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label htmlFor='nombre'>Nombre:</label>
+            <input type='text' id='nombre' name='nombre'
+              value={this.state.name}
+              onChange={ e => this.setState({ name: e.target.value })}
+              ref={ inputElemnt => this.inputName = inputElemnt }></input>
+          </p>
+          <p> 
+            <label>Apellido:</label>
+            <input type='text' id='apellido' name='apellido'
+              value={this.state.lastname}
+              onChange={ e => this.setState({ lastname : e.target.value })}></input>
+          </p>
+          <p>
+            <label>DNI:</label>
+            <input type='text' id='dni' name='dni'
+              value={this.state.dni}
+              onChange={ e => this.setState({ dni: e.target.value })}></input>
+          </p>
+          <p>
+            <label>
+              <input onChange={this.handleChange} type='checkbox' id='terminos' name='terminos'
+                checked={this.state.termns}></input>
+              Aceptar terminos y condificones
+            </label>
+          </p>
+          <button>Enviar</button>
+          {/* <button onClick={this.handleClick}>Enviar</button> */}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Forms;
