c1b3d123571dfed508a019a93dc68f95ecdc1756
Gustavo Siciliano <gussiciliano@gmail.com>
2020-05-02 01:49:07 -0300

    stateless

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-import CicloDesmontaje from './sections/cicloDesmontaje'
+import PropTypes from 'prop-types';
+// import CicloDesmontaje from './sections/cicloDesmontaje'
 //import FetchExample from './sections/fetch_example'
 //import CicloActualizacion from './sections/cicloActualizacion'
 //import AppFull from './sections/app_full'
 
+
+function MsjSimple(props) {
+  //Stateless con función
+  return(
+    <p>{ props.msj }</p>
+  )
+}
+
+MsjSimple.propTypes = {
+  msj: PropTypes.string.isRequired
+}
+
+const Msj = ({ msj1 = 'hooola', msj2 }) => (
+  //Stateless con const
+  <div>
+    <p>{ msj1 }</p>
+    <strong>{ msj2 }</strong>
+  </div>
+)
+
+Msj.defaultProps = {
+  msj2: '¿estás?'
+}
 
 class App extends Component {
   render(){
     console.log("render App")
     return (
       <div>
-        <CicloDesmontaje />
+        <h4>TEST CONDITIONAL</h4>
+        < MsjSimple msj='hola'/>
+        < Msj msj1='hello' msj2='¿cómo estás?'/>
+        < Msj />
       </div>
     );
   }
